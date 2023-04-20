e0dff101707bb5ccdc53d1e73c18661d05f7ab26
Erica <ericaemorabito@gmail.com>
2023-04-20 09:05:45 -0700

    backarrow complete

--- src/components/Backarrow.js
+++ src/components/Backarrow.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import arrow from "../images/arrow.jpeg";
+import "../styles/backarrow.css";
 
-const 
- = () => {
+const Backarrow = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Row className="back-row">
+        <Col lg={2}>
+          <a href="/Victoria_Stewart_Photography/work">
+            <img id="arrow-image" className="mx-3" src={arrow} alt="back to work arrow"></img>
+          </a>
+          <h2 id="view-work">Work</h2>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default 
+export default Backarrow;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/portfolio.css";
+import Backarrow from "./Backarrow";
 
 const Portfolio = ({
   event,
@@ -32,6 +33,7 @@ const Portfolio = ({
           <img className="portfolio-image" src={image4}></img>
         </Col>
       </Row>
+      <Backarrow />
     </Container>
   );
 };
